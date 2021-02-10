14802d419c9eea789df402b2d3f0ca57bfe7108e
aidenper <aiden.p2908@gmail.com>
2021-02-11 01:36:05 +1300

    attempt 2 to fix mail listener

--- app/email/index.js
+++ app/email/index.js
@@ -1,25 +1,11 @@
-var MailListener = require("mail-listener2");
+const MailListener = require("mail-listener2");
 const { logError, logWarn, log, logInfo } = require("../logger");
 
-var mailListener = new MailListener({
-  username: process.env.EMAIL,
-  password: process.env.EMAIL_PSW,
-  host: "outlook.office365.com",
-  port: 993, // imap port
-  tls: true,
-  connTimeout: 10000, // Default by node-imap
-  authTimeout: 5000, // Default by node-imap,
-  debug: () => {}, // Or your custom function with only one incoming argument. Default: null
-  tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email willbe marked as seen and not fetched next time
-  fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: { streamAttachments: true }, // options to be passed to mailParser lib.
-  attachments: true, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" }, // specify a download directory for attachments
-});
+let mailListener = null;
+const startDateTime = new Date();
+let counter = 0;
 
+let done = false;
 const parseAlert = (mail, onAlert) => {
   log(mail.subject);
   try {
@@ -42,28 +28,52 @@ const parseAlert = (mail, onAlert) => {
   } catch (error) {}
 };
 
-const startListening = async (onAlert) => {
-  const startDateTime = new Date();
+const createNewMailListener = (onAlert) => {
+  mailListener = new MailListener({
+    username: process.env.EMAIL,
+    password: process.env.EMAIL_PSW,
+    host: "outlook.office365.com",
+    port: 993, // imap port
+    tls: true,
+    connTimeout: 10000, // Default by node-imap
+    authTimeout: 5000, // Default by node-imap,
+    debug: () => {}, // Or your custom function with only one incoming argument. Default: null
+    tlsOptions: { rejectUnauthorized: false },
+    mailbox: "INBOX", // mailbox to monitor
+    searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
+    markSeen: true, // all fetched email willbe marked as seen and not fetched next time
+    fetchUnreadOnStart: true, // use it only if you want to get all unread email on lib start. Default is `false`,
+    mailParserOptions: { streamAttachments: true }, // options to be passed to mailParser lib.
+    attachments: true, // download attachments as they are encountered to the project directory
+    attachmentOptions: { directory: "attachments/" }, // specify a download directory for attachments
+  });
 
-  mailListener.start(); // start listening
+  mailListener.start();
 
   mailListener.on("server:connected", function () {
-    log("imapConnected");
+    log("imap connected...");
+    if (!done) {
+      setTimeout(() => {
+        log("disconecting...");
+        mailListener.stop();
+        done = true;
+      }, 2000);
+    }
   });
 
   mailListener.on("server:disconnected", function () {
-    log("imapDisconnected");
-    log("Attempting to restart");
-    // mailListener.start();
+    log("imap disconnected");
+    setTimeout(() => {
+      log("trying to reconnect...");
+      createNewMailListener(onAlert);
+      // counter++;
+      // if (counter < 2) done = false;
+    }, 1500);
   });
 
   mailListener.on("error", function (err) {
     if (err) {
       logError(`imaperror ${err}`);
-      log("Atempting to restart...");
-      setTimeout(() => {
-        mailListener.start();
-      }, 1200);
     }
   });
 
@@ -71,6 +81,10 @@ const startListening = async (onAlert) => {
     if (mail.date > startDateTime)
       if (mail.date > startDateTime) parseAlert(mail, onAlert);
   });
+};
+
+const startListening = async (onAlert) => {
+  createNewMailListener(onAlert);
 };
 
 module.exports = { startListening: startListening };
